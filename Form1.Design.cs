bd1ce994f0c8874d733b4e105ca7f2ef0833a722
ggfunction <68346218+ggfunction@users.noreply.github.com>
2021-08-02 03:51:11 +0900

    Update

--- Form1.Design.cs
+++ Form1.Design.cs
@@ -1,48 +1,59 @@
 namespace HttpListening
 {
     using System;
+    using System.Linq;
     using System.Windows.Forms;
 
     internal partial class Form1 : Form
     {
         private void InitializeComponent()
         {
-            this.Button1 = new Button
+            this.Label1 = new Label
             {
-                Text = "Server.Start()",
+                Text = "http://localhost:{Port}/",
                 Left = 8,
                 Top = 8,
-                Width = 100,
+                Width = 130,
+                AutoSize = true,
             };
 
-            this.Button2 = new Button
+            this.Label2 = new Label
             {
-                Text = "Server.Stop()",
-                Left = this.Button1.Left + this.Button1.Width + 8,
-                Top = this.Button1.Top,
-                Width = this.Button1.Width,
-            };
-
-            this.Button3 = new Button
-            {
-                Text = "Server.Close()",
-                Left = this.Button2.Left + this.Button2.Width + 8,
-                Top = this.Button1.Top,
-                Width = this.Button1.Width,
-            };
-
-            this.Button4 = new Button
-            {
-                Text = "launch browser",
-                Left = this.Button1.Left,
-                Top = this.Button1.Top + this.Button1.Height + 8,
-                Width = this.Button1.Width,
+                Text = "ContentRoot",
+                Left = this.Label1.Left,
+                Top = this.Label1.Top + this.Label1.Height,
             };
 
             this.TextBox1 = new TextBox
             {
-                Left = this.Button1.Left,
-                Top = this.Button4.Top + this.Button4.Height + 8,
+                Left = this.Label1.Left + this.Label1.Width,
+                Top = this.Label1.Top,
+                Width = 48,
+            };
+
+            this.TextBox1.KeyPress += this.TextBox1_KeyPress;
+            this.TextBox1.Leave += this.TextBox1_Leave;
+
+            this.Label3 = new Label
+            {
+                Text = "Concurrent",
+                Left = this.TextBox1.Left + this.TextBox1.Width + 24,
+                Top = this.Label1.Top,
+                Width = 60,
+                AutoSize = true,
+            };
+
+            this.TextBox2 = new TextBox
+            {
+                Left = this.Label1.Left,
+                Top = this.Label1.Top + this.Label1.Height + 16,
+                Width = 250,
+            };
+
+            this.TextBox3 = new TextBox
+            {
+                Left = this.Label1.Left,
+                Top = this.TextBox2.Top + this.TextBox2.Height + 8,
                 Width = 300,
                 Height = 100,
                 Multiline = true,
@@ -50,8 +61,75 @@ namespace HttpListening
                 ScrollBars = ScrollBars.Vertical,
             };
 
-            this.Controls.AddRange(new Control[] { this.Button1, this.Button2, this.Button3, this.Button4, this.TextBox1 });
-            this.ClientSize = new System.Drawing.Size(this.Button3.Left + this.Button3.Width + 24, this.ClientSize.Height);
+            this.Button1 = new Button
+            {
+                Text = "browse",
+                Left = this.TextBox2.Left + this.TextBox2.Width + 8,
+                Top = this.TextBox2.Top,
+                Width = 64,
+                Tag = new object(),
+            };
+
+            this.Button1.Click += this.Button1_Click;
+
+            this.Button2 = new Button
+            {
+                Text = "Server.Start()",
+                Left = this.Label1.Left,
+                Top = this.TextBox3.Top + this.TextBox3.Height + 8,
+                Width = 100,
+            };
+
+            this.Button2.Click += this.Button2_Click;
+
+            this.Button3 = new Button
+            {
+                Text = "Server.Stop()",
+                Left = this.Button2.Left + this.Button2.Width + 8,
+                Top = this.Button2.Top,
+                Width = this.Button2.Width,
+            };
+
+            this.Button3.Click += this.Button3_Click;
+
+            this.Button4 = new Button
+            {
+                Text = "Server.Close()",
+                Left = this.Button3.Left + this.Button3.Width + 8,
+                Top = this.Button2.Top,
+                Width = this.Button2.Width,
+            };
+
+            this.Button4.Click += this.Button4_Click;
+
+            this.Button5 = new Button
+            {
+                Text = "launch browser",
+                Left = this.Button2.Left,
+                Top = this.Button2.Top + this.Button2.Height + 8,
+                Width = this.Button2.Width,
+            };
+
+            this.Button5.Click += this.Button5_Click;
+
+            this.ComboBox1 = new ComboBox
+            {
+                Left = this.Label3.Left + this.Label3.Width + 8,
+                Top = this.TextBox1.Top,
+                Width = 48,
+                DropDownStyle = ComboBoxStyle.DropDownList,
+                DataSource = Enumerable.Range(1, Environment.ProcessorCount).Reverse().ToArray(),
+            };
+
+            this.ComboBox1.SelectedIndexChanged += this.ComboBox1_SelectedIndexChanged;
+
+            this.Controls.AddRange(new Control[] { this.Button1, this.Button2, this.Button3, this.Button4, this.Button5 });
+            this.Controls.AddRange(new Control[] { this.TextBox1, this.TextBox2, this.TextBox3 });
+            this.Controls.AddRange(new Control[] { this.Label1, this.Label2, this.Label3 });
+            this.Controls.AddRange(new Control[] { this.ComboBox1 });
+
+            this.ClientSize = new System.Drawing.Size(this.Button4.Left + this.Button4.Width + 24, this.ClientSize.Height);
+            this.MaximizeBox = false;
             this.Text = Application.ProductName;
         }
     }
--- Form1.cs
+++ Form1.cs
@@ -6,51 +6,39 @@ namespace HttpListening
 
     internal partial class Form1 : Form
     {
-        private readonly CheapHttpServer server;
+        private CheapHttpServer server;
+
+        private State state;
 
         public Form1()
         {
             this.InitializeComponent();
 
-            this.server = new CheapHttpServer();
-
             var addLog = new EventHandler((s, e) =>
             {
                 var text = string.Format("{0} {1}", DateTime.Now, ((Control)s).Text);
-                this.TextBox1.AppendText(text);
-                this.TextBox1.AppendText(Environment.NewLine);
+                this.TextBox3.AppendText(text);
+                this.TextBox3.AppendText(Environment.NewLine);
             });
 
-            this.Button1.Click += (s, e) =>
-            {
-                this.server.Start();
-            };
-
-            this.Button2.Click += (s, e) =>
-            {
-                this.server.Stop();
-            };
-
-            this.Button3.Click += (s, e) =>
-            {
-                this.server.Close();
-            };
-
-            this.Button4.Click += (s, e) =>
-            {
-                var info = new System.Diagnostics.ProcessStartInfo
-                {
-                    FileName = "rundll32.exe",
-                    Arguments = "url.dll,FileProtocolHandler http://localhost:8080/",
-                    CreateNoWindow = false,
-                };
-
-                System.Diagnostics.Process.Start(info);
-            };
+            this.Controls.OfType<Button>()
+                .Where(x => x.Tag == null)
+                .ToList()
+                .ForEach(x => x.Click += addLog);
 
             this.Controls.OfType<Button>()
                 .ToList()
-                .ForEach(x => x.Click += addLog);
+                .ForEach(x => x.TabStop = false);
+
+            this.AcceptCommandLineArgs();
+            this.ResetState();
+        }
+
+        private enum State
+        {
+            Closed,
+            Started,
+            Stopped,
         }
 
         public Button Button1 { get; private set; }
@@ -61,15 +49,200 @@ namespace HttpListening
 
         public Button Button4 { get; private set; }
 
+        public Button Button5 { get; private set; }
+
         public TextBox TextBox1 { get; private set; }
+
+        public TextBox TextBox2 { get; private set; }
+
+        public TextBox TextBox3 { get; private set; }
+
+        public Label Label1 { get; private set; }
+
+        public Label Label2 { get; private set; }
+
+        public Label Label3 { get; private set; }
+
+        public ComboBox ComboBox1 { get; private set; }
 
         protected override void OnFormClosed(FormClosedEventArgs e)
         {
-            this.server.Dispose();
+            this.CloseServer();
         }
 
         protected override void OnShown(EventArgs e)
         {
+        }
+
+        private void AcceptCommandLineArgs()
+        {
+            var commandLineArgs = Environment.GetCommandLineArgs().Skip(1);
+
+            int i;
+            string port = commandLineArgs.FirstOrDefault(x => int.TryParse(x, out i));
+            if (string.IsNullOrEmpty(port))
+            {
+                port = "8080";
+            }
+
+            this.TextBox1.Text = System.Text.RegularExpressions.Regex.Replace(port, "[^0-9]", string.Empty);
+
+            var contentRoot = commandLineArgs.FirstOrDefault(x => System.IO.Directory.Exists(x));
+            if (string.IsNullOrEmpty(contentRoot))
+            {
+                contentRoot = Environment.CurrentDirectory;
+            }
+
+            this.TextBox2.Text = contentRoot;
+        }
+
+        private void AppendLog(object obj, string note)
+        {
+            var target = (obj as Control) != null ?
+                (obj as Control).Text : string.Empty;
+            var text = string.Format("{0} {1} {2}", DateTime.Now, target, note);
+            this.TextBox3.AppendText(text);
+            this.TextBox3.AppendText(Environment.NewLine);
+        }
+
+        private void CloseServer()
+        {
+            if (this.state == State.Closed)
+            {
+                return;
+            }
+
+            this.server.Dispose();
+            this.SetState(State.Closed);
+        }
+
+        private void ResetState()
+        {
+            this.SetState(State.Closed);
+        }
+
+        private void SetState(State value)
+        {
+            this.TextBox1.ReadOnly = !(value == State.Closed);
+            this.TextBox2.ReadOnly = !(value == State.Closed);
+            this.Button1.Enabled = value == State.Closed;
+            this.Button2.Enabled = value != State.Started;
+            this.Button3.Enabled = value == State.Started;
+            this.Button4.Enabled = value != State.Closed;
+            this.Button5.Enabled = !(value == State.Closed);
+            this.state = value;
+        }
+
+        private void StartServer()
+        {
+            if (this.state == State.Started)
+            {
+                return;
+            }
+
+            if (this.state == State.Closed)
+            {
+                var port = string.IsNullOrEmpty(this.TextBox1.Text) ?
+                    0 : Convert.ToInt32(this.TextBox1.Text);
+                var contentRoot = this.TextBox2.Text;
+                var concurrent = Convert.ToInt32(this.ComboBox1.Text);
+                try
+                {
+                    this.server = new CheapHttpServer(contentRoot, port)
+                    {
+                        ConcurrentRequests = concurrent,
+                    };
+
+                    this.TextBox1.Text = this.server.Port.ToString();
+                }
+                catch (Exception ex)
+                {
+                    this.TextBox3.AppendText(ex.Message);
+                    this.TextBox3.AppendText(Environment.NewLine);
+                    this.server = null;
+                    return;
+                }
+            }
+
+            this.server.Start();
+            this.SetState(State.Started);
+        }
+
+        private void StopServer()
+        {
+            if (this.state == State.Started)
+            {
+                this.server.Stop();
+                this.SetState(State.Stopped);
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            using (var dialog = new FolderBrowserDialog())
+            {
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    this.TextBox2.Text = dialog.SelectedPath;
+                }
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            this.StartServer();
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            this.StopServer();
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            this.CloseServer();
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            if (this.server == null)
+            {
+                return;
+            }
+
+            var port = this.server.Port;
+            var info = new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = "rundll32.exe",
+                Arguments = string.Format("url.dll,FileProtocolHandler http://localhost:{0}/", port),
+                CreateNoWindow = false,
+            };
+
+            System.Diagnostics.Process.Start(info);
+        }
+
+        private void TextBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar);
+        }
+
+        private void TextBox1_Leave(object sender, EventArgs e)
+        {
+            var text = (sender as TextBox).Text;
+
+            if (System.Text.RegularExpressions.Regex.IsMatch(text, "[^0-9]"))
+            {
+                (sender as TextBox).Text = System.Text.RegularExpressions.Regex.Replace(text, "[^0-9]", string.Empty);
+            }
+        }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (this.state != State.Closed)
+            {
+                var concurrent = Convert.ToInt32(this.ComboBox1.Text);
+                this.server.ConcurrentRequests = concurrent;
+            }
         }
     }
 }
